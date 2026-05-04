1c1f2c9d8396e612d0c47c4ce664f6c555129392
mero93m <merabkopaleishvili1993@gmail.com>
2026-05-04 13:41:07 +0400

    feat: add footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,17 @@ import { RouterOutlet } from '@angular/router';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, BreadcrumbsComponent, ScrollToTopComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    ScrollToTopComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
